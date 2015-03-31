3d01dca90d77bd20e7d6daec60cf9cdf37b31c63
Patrick Huck <patrick@the-huck.com>
2015-03-31 15:04:55 -0700

    start on mp_cite_refs

---
+++ mp_cite_refs.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+import subprocess, json
+
+scholar = './scholar.py'
+titles = [
+    'A high-throughput infrastructure for density functional theory calculations',
+    'The Materials Project: A materials genome approach to accelerating materials innovation',
+]
+
+for title in titles:
+    print title
+    parent_pub = json.loads(subprocess.check_output([
+        scholar, '-c', '1', '-p', title, '-t', '--json_form'
+    ]))
+    print '\t#citations = {}'.format(parent_pub['num_citations'])
+    cluster_id = parent_pub['cluster_id'] #'9071217946286299275'
+    raw_cites = subprocess.check_output([
+        scholar, '--cites', cluster_id, '--json_form'
+    ])
+    child_pubs = json.loads('\n'.join([
+        '[', raw_cites.replace('}\n{', '},\n{'), ']'
+    ]))
+    print '\t#child_pubs = {}'.format(len(child_pubs))
+    child_pubs_pdf = filter(None, [
+        child_pub['url_pdf'] for child_pub in child_pubs
+    ])
+    print '\t#child_pubs_pdf = {}'.format(len(child_pubs_pdf))
+    #break
